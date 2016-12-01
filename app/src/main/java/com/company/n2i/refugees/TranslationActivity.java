59ecf9d800749caeae23f1625c2adfbc3cd1da3c
lucas <biblion84@free.fr>
2016-12-01 21:21:15 +0100

    les boutons sont clickables

--- app/src/main/java/com/company/n2i/refugees/TranslationActivity.java
+++ app/src/main/java/com/company/n2i/refugees/TranslationActivity.java
@@ -2,13 +2,46 @@ package com.company.n2i.refugees;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class TranslationActivity extends AppCompatActivity {
+
+
+    ImageButton native_flag_button;
+    ImageButton destination_flag_button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_translation);
+        addListenerOnFlagButton();
+    }
+
+
+    public void addListenerOnFlagButton() {
+
+        native_flag_button = (ImageButton) findViewById(R.id.native_language_flag);
+        native_flag_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Toast.makeText(TranslationActivity.this,
+                        "ImageButton is clicked!", Toast.LENGTH_SHORT).show();
+            }
+
+        });
+
+        destination_flag_button = (ImageButton) findViewById(R.id.destination_language_flag);
+        destination_flag_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Toast.makeText(TranslationActivity.this,
+                        "Destination is clicked!", Toast.LENGTH_SHORT).show();
+            }
+
+        });
 
     }
 }
