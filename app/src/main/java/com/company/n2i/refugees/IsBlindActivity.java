654fdf8b4a18398344cfbe1bba1e0c726833b1d8
sideness <julien.stefani.fr@gmail.com>
2016-12-01 22:36:02 +0100

    Ajout du set button

--- app/src/main/java/com/company/n2i/refugees/IsBlindActivity.java
+++ app/src/main/java/com/company/n2i/refugees/IsBlindActivity.java
@@ -1,7 +1,11 @@
 package com.company.n2i.refugees;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 public class IsBlindActivity extends AppCompatActivity {
 
@@ -9,5 +13,26 @@ public class IsBlindActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_is_blind);
+
+        RelativeLayout lnBlindMode = (RelativeLayout) findViewById(R.id.rlBlindMode);
+        RelativeLayout lnNormalMode = (RelativeLayout) findViewById(R.id.rlNormalMode);
+
+        lnBlindMode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO setglobal variable for blind mode
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        lnNormalMode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO setglobal variable for blind mode
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
